604b4e05042f2fc13ad0cbfe934e55392100e8bf
cytxiwai <98063373+cytxiwai@users.noreply.github.com>
2022-08-21 11:43:51 -0700

    initial

---
+++ HRAssistant/src/test/java/tc/HRAssistant/HrAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package tc.HRAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
